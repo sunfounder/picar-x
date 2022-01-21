da3af5ee6e5a55534958158df54c269c64721001
EverardoG <everardo.a.gonzalez@gmail.com>
2022-01-21 13:13:47 -0800

    fix keyboard interface

--- lib/keyboard_interface.py
+++ lib/keyboard_interface.py
@@ -4,18 +4,33 @@ from time import sleep
 import logging
 import sys
 from termios import tcflush, TCIFLUSH
+from typing import Tuple
 from picarx_improved import Picarx
 from maneuvering import LEFT, RIGHT, k_turn, parallel_park, move
 
 logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO,
     datefmt="%H:%M:%S")
 
-def parse_angle(input_str: str)->int:
-    if len(input_str.split(" ")) == 2 and input_str.split(" ")[1].isdecimal():
-        angle = int(input_str.split(" ")[1])
+def is_integer(string: str)->bool:
+    if string[0] == "-" and string[1:].isdigit():
+        return True
+    elif string.isdigit():
+        return True
     else:
+        return False
+
+def parse_angle(input_str: str)->Tuple[int, bool]:
+    error = False
+    if len(input_str.split(" ")) == 2 and is_integer(input_str.split(" ")[1]):
+        angle = int(input_str.split(" ")[1])
+    elif len(input_str.split(" ")) == 1:
+        logging.info("No angle specified. Setting angle to 0.")
         angle = 0
-    return angle
+    else:
+        logging.error(f"({ input_str.split(' ')[1] }) is not an integer angle. Setting angle to 0.")
+        angle = 0
+        error = True
+    return angle, error
 
 def main()->None:
     print(
@@ -31,7 +46,7 @@ def main()->None:
     # Take user input until receiving shutdown command
     user_input = ""
     while user_input != "shutdown":
-        # Flush any extraneous keyboard input 
+        # Flush any extraneous keyboard input
         tcflush(sys.stdin, TCIFLUSH)
         # Take in command as user_input
         user_input = input("\nInput: ")
@@ -49,8 +64,10 @@ def main()->None:
             logging.info("Parallel parking right.")
             parallel_park(px, RIGHT)
         elif user_input.split(" ")[0] == "forward" or user_input == "forward":
-            angle = parse_angle(user_input)
-            if angle == 0:
+            angle, error = parse_angle(user_input)
+            if error:
+                logging.error("Input angle was not valid. Car will not move.")
+            elif angle == 0:
                 logging.info(f"Moving forward.")
             else:
                 logging.info(f"Moving forward with angle {angle}")
@@ -58,8 +75,10 @@ def main()->None:
             sleep(2)
             move(px, 0, 0)
         elif user_input.split(" ")[0] == "backward" or user_input == "backward":
-            angle = parse_angle(user_input)
-            if angle == 0:
+            angle, error = parse_angle(user_input)
+            if error:
+                logging.error("Input angle was not valid. Car will not move.")
+            elif angle == 0:
                 logging.info(f"Moving backward")
             else:
                 logging.info(f"Moving backward with angle {angle}")
