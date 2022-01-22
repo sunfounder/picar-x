fix sign

lib/read_sensors.py
@@ -6,9 +6,9 @@ while True:
     raw_reading = np.asarray(px.ir_sensors.read())
     # print(raw_reading)
     state = np.average(np.diff(raw_reading))
-    if state > 0:
+    if state < 0:
         print("LEFT")
-    elif state < 0:
+    elif state > 0:
         print("RIGHT")
     else:
         print("FORWARD")