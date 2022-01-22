a4b326b67b54245cd03e88dc1cd85c8794196b0a
EverardoG <everardo.a.gonzalez@gmail.com>
2022-01-22 15:51:10 -0800

    print command

--- lib/read_sensors.py
+++ lib/read_sensors.py
@@ -4,11 +4,11 @@ from picarx_improved import Picarx
 px = Picarx()
 while True:
     raw_reading = np.asarray(px.ir_sensors.read())
-    print(raw_reading)
-    # state = np.average(np.diff(raw_reading))
-    # if state > 0:
-    #     print("LEFT")
-    # elif state < 0:
-    #     print("RIGHT")
-    # else:
-    #     print("FORWARD")
+    # print(raw_reading)
+    state = np.average(np.diff(raw_reading))
+    if state > 0:
+        print("LEFT")
+    elif state < 0:
+        print("RIGHT")
+    else:
+        print("FORWARD")
