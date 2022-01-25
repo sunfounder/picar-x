b083eb5c5643164d32efd801fae695ade5a68867
EverardoG <everardo.a.gonzalez@gmail.com>
2022-01-25 21:08:56 +0000

    add test images

---
+++ example/take_pic.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import os
+
+os.environ['DISPLAY'] = ':0'
+
+#init camera
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 24
+rawCapture = PiRGBArray(camera, size=camera.resolution)  
+
+
+for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True): # use_video_port=True
+    img = frame.array
+    cv2.imshow("video", img)  # OpenCV image show
+    rawCapture.truncate(0)  # Release cache
+    cv2.imwrite('/home/egonzalez/picar-x/test_images/img2.jpg', img) # Save image
+
+    # click ESC key to exit.
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        camera.close()
+        break
+
+        
