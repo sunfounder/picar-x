1f2783cc8f68aa484614f445ac25ee573fb12d19
EverardoG <everardo.a.gonzalez@gmail.com>
2022-01-27 16:56:33 -0800

    implement read-write lock

--- lib/bus.py
+++ lib/bus.py
@@ -1,10 +1,17 @@
+from email import message
+from readerwriterlock import rwlock
+
 class Bus():
     def __init__(self, message)->None:
+        self.lock = rwlock.RWLockWriteD()
         self.message = message
 
     def read(self):
-        return self.message
+        with self.lock.gen_rlock():
+            message = self.message
+        return message
 
     def write(self, message)->None:
-        self.message = message
+        with self.lock.gen_wlock():
+            self.message = message
         return None
