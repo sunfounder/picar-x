9a68864262a2c8de87ff2b19f7f19380da3364f6
Lovmong <1967471283@qq.com>
2023-06-26 18:00:39 +0800

    optimize obstacle avoidance logic

--- example/avoiding_obstacles.py
+++ example/avoiding_obstacles.py
@@ -1,19 +1,31 @@
 from picarx import Picarx
+import time
 
+POWER = 50
+SafeDistance = 40   # > 30 safe
+DangerDistance = 20 # > 20 && < 30 turn around, 
+                    # < 20 backward
 
 def main():
     try:
         px = Picarx()
         # px = Picarx(ultrasonic_pins=['D2','D3']) # tring, echo
-        px.forward(30)
+       
         while True:
-            distance = px.ultrasonic.read()
+            distance = round(px.ultrasonic.read(), 2)
             print("distance: ",distance)
-            if distance > 0 and distance < 300:
-                if distance < 25:
-                    px.set_dir_servo_angle(-35)
-                else:
-                    px.set_dir_servo_angle(0)
+            if distance >= SafeDistance:
+                px.set_dir_servo_angle(0)
+                px.forward(POWER)
+            elif distance >= DangerDistance:
+                px.set_dir_servo_angle(40)
+                px.forward(POWER)
+                time.sleep(0.1)
+            else:
+                px.set_dir_servo_angle(-40)
+                px.backward(POWER)
+                time.sleep(0.5)
+
     finally:
         px.forward(0)
 
