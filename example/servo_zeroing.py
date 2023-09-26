3d1850b921d8cd53f51a43cf3efabf7a27d2ea86
Lovmong <1967471283@qq.com>
2023-09-26 13:59:35 +0800

    optimize

--- example/servo_zeroing.py
+++ example/servo_zeroing.py
@@ -1,5 +1,5 @@
 
-from robot_hat import PWM, Servo
+from robot_hat import Servo
 from robot_hat.utils import reset_mcu
 from time import sleep
 
@@ -7,13 +7,12 @@ reset_mcu()
 sleep(0.2)
 
 if __name__ == '__main__':
-    servos_pin = ['P0', 'P1', 'P2']
     print(f"Set servo to zero")
-    for pin in servos_pin:
-        # print(f"Servo {pin} set to zero")
-        Servo(pin).angle(10)
+    for i in range(12):
+        # print(f"Servo {i} set to zero")
+        Servo(i).angle(10)
         sleep(0.1)
-        Servo(pin).angle(0)
+        Servo(i).angle(0)
         sleep(0.1)
     while True:
         sleep(1)
