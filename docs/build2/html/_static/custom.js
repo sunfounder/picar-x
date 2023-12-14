67e9a92e37dedd92c3a10d32d1b580ab1fac730a
zhuang <zhuang201704@gmail.com>
2023-12-14 17:46:38 +0800

    Delete the build2 file

--- docs/build2/html/_static/custom.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll('a[href^="https://docs.sunfounder.com/projects/ezblock3"]');
-    links.forEach(function(link) {
-        link.setAttribute('target', '_blank');
-    });
-});
