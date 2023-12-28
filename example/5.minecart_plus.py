b84ddcd5ad117778f19f02e50411eab690d6b5ef
Lovmong <1967471283@qq.com>
2023-12-28 11:43:28 +0800

    optimize, add outHandle()

--- example/5.minecart_plus.py
+++ example/5.minecart_plus.py
@@ -17,12 +17,32 @@ from time import sleep
 
 px = Picarx()
 # px = Picarx(grayscale_pins=['A0', 'A1', 'A2'])
-# manual modify reference value
-px.set_line_reference([500, 600, 600])
+
+# Please run ./calibration/grayscale_calibration.py to Auto calibrate grayscale values
+# or manual modify reference value by follow code
+# px.set_line_reference([1400, 1400, 1400])
 
 current_state = None
 px_power = 10
 offset = 20
+last_state = "stop"
+
+def outHandle():
+    global last_state, current_state
+    if last_state == 'left':
+        px.set_dir_servo_angle(-30)
+        px.backward(10)
+    elif last_state == 'right':
+        px.set_dir_servo_angle(30)
+        px.backward(10)
+    while True:
+        gm_val_list = px.get_grayscale_data()
+        gm_state = get_status(gm_val_list)
+        print("outHandle gm_val_list: %s, %s"%(gm_val_list, gm_state))
+        currentSta = gm_state
+        if currentSta != last_state:
+            break
+    sleep(0.001)
 
 def get_status(val_list):
     _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
@@ -42,9 +62,10 @@ if __name__=='__main__':
             gm_state = get_status(gm_val_list)
             print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
 
-            if gm_state == "stop":
-                px.stop()
-            elif gm_state == 'forward':
+            if gm_state != "stop":
+                last_state = gm_state
+
+            if gm_state == 'forward':
                 px.set_dir_servo_angle(0)
                 px.forward(px_power) 
             elif gm_state == 'left':
@@ -53,10 +74,9 @@ if __name__=='__main__':
             elif gm_state == 'right':
                 px.set_dir_servo_angle(-offset)
                 px.forward(px_power) 
+            else:
+                outHandle()
     finally:
         px.stop()
         print("stop and exit")
         sleep(0.1)
-
-
-                
