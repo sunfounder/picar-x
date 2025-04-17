3d5936b5e347803f6a5f708700142a79b25badc9
Cavon <cavonxx@gmail.com>
2025-04-17 11:37:08 +0800

    refactor(picarx_app_control): extract command handling logic into separate functions
    The changes refactor the `commands__handler` function by extracting command handling logic into individual functions for better maintainability and readability. This reduces code duplication and improves the structure of the application. Additionally, the `on_receive` function now includes a check for valid command formats, and the `update_data` function has been separated from the `main` loop for better organization.

--- mammoth/picarx_app_control.py
+++ mammoth/picarx_app_control.py
@@ -97,6 +97,90 @@ except Exception as e:
     vilib_obj_error.add(str(e))
     print(e)
 
+def constrain(value, min_value, max_value):
+    return min(max(value, min_value), max_value)
+
+def handle_motor(values):
+    px.set_motor_speed(1, values[0])
+    px.set_motor_speed(2, values[1])
+
+def handle_steering(values):
+    px.set_dir_servo_angle(values[0])
+
+def handle_camera_pan(values):
+    px.set_cam_pan_angle(values[0])
+
+def handle_camera_tilt(values):
+    px.set_cam_tilt_angle(values[0])
+
+def handle_color_detection_switch(values):
+    Vilib.color_detect(values[0])
+
+def handle_face_detection_switch(values):
+    Vilib.face_detect_switch(values[0])
+
+def handle_traffic_sign_detection_switch(values):
+    Vilib.traffic_detect_switch(values[0])
+
+def handle_qr_code_detection_switch(values):
+    Vilib.qrcode_detect_switch(values[0])
+
+def handle_front_sound_effect(values):
+    if values[0]!= 0 and not music.get_sound_busy():
+        music.play_sound_effect(picarx_sounds[values[0]-1])
+
+def handle_background_music(values):
+    if values[0]!= 0 and not music.get_music_busy():
+        music.play_music(picarx_musics[values[0]-1])
+
+def handle_background_music_control(values):
+    music.music_control(values[0])
+
+def handle_background_music_volume(values):
+    music.set_music_volume(values[0])
+
+def handle_track_mode(values):
+    mode = values[0]
+    speed = values[1]
+    if mode == 0: # stop
+        px.stop()
+    elif mode == 1: # forward
+        line_track(speed)
+
+def handle_obstacle_mode(values):
+    mode = values[0]
+    speed = values[1]
+    if mode == 0: # stop
+        px.stop()
+    elif mode == 1: # forward
+        avoid_obstacles(speed)
+
+def handle_servo_calibation(values):
+    servo_index = values[0]
+    direction = values[1]
+    offset = None
+    if servo_index == 0: # camera pan servo
+        offset = px.cam_pan_cali_val + direction * 2
+        offset = constrain(offset, -200, 200)
+        px.cam_pan_servo_calibrate(offset)
+    elif servo_index == 1: # camera tilt servo
+        offset = px.cam_tilt_cali_val + direction * 2
+        offset = constrain(offset, -200, 200)
+        px.cam_tilt_servo_calibrate(offset)
+    elif servo_index == 2: # dir servo 1
+        offset = px.dir_cali_val + direction * 2
+        offset = constrain(offset, -200, 200)
+        px.dir_servo_calibrate(offset)
+    if offset != None:
+        sensors['servos_offset']['value'][servo_index] = offset
+
+def handle_motors_calibation(values):
+    px.motor_direction_calibrate(1, values[0])
+    px.motor_direction_calibrate(2, values[1])
+    px.forward(30)
+    time.sleep(2)
+    px.stop()
+
 # main
 # =================================================================
 def commands__handler(_command_entities):
@@ -104,153 +188,109 @@ def commands__handler(_command_entities):
     # ------------------------------------
     # --- motors ---
     if 'motor' in _command_entities.keys():
-        if _command_entities['motor']['value'] != commands['motor']['last_value']:
-            _motor = _command_entities['motor']['value']
-            commands['motor']['last_value'] = _motor
-            px.set_motor_speed(1, _motor[0])
-            px.set_motor_speed(2, _motor[1])
+        values = _command_entities['motor']['value']
+        handle_motor(values)
     # --- steering servo ---
     if 'steering' in _command_entities.keys():
-        if _command_entities['steering']['value'] != commands['steering']['last_value']:
-            _steering = _command_entities['steering']['value']
-            commands['steering']['last_value'] = _steering
-            px.set_dir_servo_angle(_steering[0])
+        values = _command_entities['steering']['value']
+        handle_steering(values)
     # --- camera pan servo ---
     if 'camera_pan' in _command_entities.keys():
-        if _command_entities['camera_pan']['value'] != commands['camera_pan']['last_value']:
-            _camera_pan = _command_entities['camera_pan']['value']
-            commands['camera_pan']['last_value'] = _camera_pan
-            px.set_cam_pan_angle(_camera_pan[0])
+        values = _command_entities['camera_pan']['value']
+        handle_camera_pan(values)
     # --- camera tilt servo ---
     if 'camera_tilt' in _command_entities.keys():
-        if _command_entities['camera_tilt']['value'] != commands['camera_tilt']['last_value']:
-            _camera_tilt = _command_entities['camera_tilt']['value']
-            commands['camera_tilt']['last_value'] = _camera_tilt
-            px.set_cam_tilt_angle(_camera_tilt[0])
+        values = _command_entities['camera_tilt']['value']
+        handle_camera_tilt(values)
         
     # vision processing
     # ------------------------------------
-    # ---
+    # --- color detection ---
     if 'color_detection_switch' in _command_entities.keys():
-        if _command_entities['color_detection_switch']['value'] != commands['color_detection_switch']['last_value']:
-            _color_switch = _command_entities['color_detection_switch']['value']
-            commands['color_detection_switch']['last_value'] = _color_switch
-            _color_switch = color_detection_command[_color_switch[0]]
-            Vilib.color_detect(_color_switch)
-    # ---
+        values = _command_entities['color_detection_switch']['value']
+        handle_color_detection_switch(values)
+    # --- face detection ---
     if 'face_detection_switch' in _command_entities.keys():
-        if _command_entities['face_detection_switch']['value'] != commands['face_detection_switch']['last_value']:
-            _face_switch = _command_entities['face_detection_switch']['value']
-            commands['face_detection_switch']['last_value'] = _face_switch
-            Vilib.face_detect_switch(_face_switch[0])
-    # ---
+        values = _command_entities['face_detection_switch']['value']
+        handle_face_detection_switch(values)
+    # --- traffic sign detection ---
     if 'traffic_sign_detection_switch' in _command_entities.keys():
-        if _command_entities['traffic_sign_detection_switch']['value'] != commands['traffic_sign_detection_switch']['last_value']:
-            _traffic_sign_switch = _command_entities['traffic_sign_detection_switch']['value']
-            commands['traffic_sign_detection_switch']['last_value'] = _traffic_sign_switch
-            Vilib.traffic_detect_switch(_traffic_sign_switch[0])
-    # ---
+        values = _command_entities['traffic_sign_detection_switch']['value']
+        handle_traffic_sign_detection_switch(values)
+    # --- qr code detection ---
     if 'qr_code_detection_switch' in _command_entities.keys():
-        if _command_entities['qr_code_detection_switch']['value'] != commands['qr_code_detection_switch']['last_value']:
-            _qr_code_switch = _command_entities['qr_code_detection_switch']['value']
-            commands['qr_code_detection_switch']['last_value'] = _qr_code_switch
-            Vilib.qrcode_detect_switch(_qr_code_switch[0])
+        values = _command_entities['qr_code_detection_switch']['value']
+        handle_qr_code_detection_switch(values)
 
     # sound and music
     # ------------------------------------
     # --- sound effect ---
     if 'front_sound_effect' in _command_entities.keys():
-        if _command_entities['front_sound_effect']['value'][0] != 0 and not music.get_sound_busy():
-            _sound_effect_index = _command_entities['front_sound_effect']['value'][0]
-            music.play_sound_effect(picarx_sounds[_sound_effect_index-1])
-
+        values = _command_entities['front_sound_effect']['value']
+        handle_front_sound_effect(values)
     # --- music ---
-    if 'background_music' in _command_entities.keys() or 'background_music_control' in _command_entities.keys() or 'background_music_volume' in _command_entities.keys():
-        _music_index = commands['background_music']['value']
-        _last_music_index = commands['background_music']['last_value']
-        _music_control = commands['background_music_control']['value']
-        _last_music_control = commands['background_music_control']['last_value']
-        _music_volume = commands['background_music_volume']['value']
-        _last_music_volume = commands['background_music_volume']['last_value']
+    if 'background_music' in _command_entities.keys():
+        values = _command_entities['background_music']['value']
+        handle_background_music(values)
+    # --- music control ---
+    if 'background_music_control' in _command_entities.keys():
+        values = _command_entities['background_music_control']['value']
+        handle_background_music_control(values)
+    # --- music volume ---
+    if 'background_music_volume' in _command_entities.keys():
+        values = _command_entities['background_music_volume']['value']
+        handle_background_music_volume(values)
+    # if 'background_music' in _command_entities.keys() or 'background_music_control' in _command_entities.keys() or 'background_music_volume' in _command_entities.keys():
+    #     _music_index = commands['background_music']['value']
+    #     _last_music_index = commands['background_music']['last_value']
+    #     _music_control = commands['background_music_control']['value']
+    #     _last_music_control = commands['background_music_control']['last_value']
+    #     _music_volume = commands['background_music_volume']['value']
+    #     _last_music_volume = commands['background_music_volume']['last_value']
 
-        print(_music_index, _last_music_index, _last_music_control[0])
+    #     print(_music_index, _last_music_index, _last_music_control[0])
 
-        if _music_control[0] == 1:
-            if (_last_music_control[0] == 0) or _music_index != _last_music_index:
-                commands['background_music']['last_value'] = list(_music_index)
-                _msic_path = picarx_musics[_music_index[0]-1]
-                music.play_music(_msic_path)
-                sensors['background_music_pos']['value'][0] = music.get_music_length(_msic_path)
+    #     if _music_control[0] == 1:
+    #         if (_last_music_control[0] == 0) or _music_index != _last_music_index:
+    #             commands['background_music']['last_value'] = list(_music_index)
+    #             _msic_path = picarx_musics[_music_index[0]-1]
+    #             music.play_music(_msic_path)
+    #             sensors['background_music_pos']['value'][0] = music.get_music_length(_msic_path)
 
-        if _music_volume != _last_music_volume:
-            music.set_music_volume(_music_volume[0])
-        if _music_control != _last_music_control:
-            music.music_control(_music_control[0])
-        #
-        commands['background_music_control']['last_value'] = list(_music_control)
-        commands['background_music_volume']['last_value'] = list(_music_volume)
-        #
-        if music.get_music_busy():
-            sensors['background_music_status']['value'] = list(_music_control)
-        else:
-            sensors['background_music_status']['value'] = [0]
-
-    if music.get_music_busy():
-        sensors['background_music_pos']['value'][1] = music.get_music_pos()
-
+    #     if _music_volume != _last_music_volume:
+    #         music.set_music_volume(_music_volume[0])
+    #     if _music_control != _last_music_control:
+    #         music.music_control(_music_control[0])
+    #     #
+    #     commands['background_music_control']['last_value'] = list(_music_control)
+    #     commands['background_music_volume']['last_value'] = list(_music_volume)
+    #     #
+    #     if music.get_music_busy():
+    #         sensors['background_music_status']['value'] = list(_music_control)
+    #     else:
+    #         sensors['background_music_status']['value'] = [0]
 
     # track_mode
     # ------------------------------------
     if 'track_mode' in _command_entities.keys():
-        if _command_entities['track_mode']['value'] != commands['track_mode']['last_value']:
-            _trace_mode = _command_entities['track_mode']['value']
-            _track_speed = _command_entities['track_mode']['value'][1]
-            commands['track_mode']['last_value'] = _trace_mode
-            if _trace_mode[0] == 0:
-                px.stop()
-        if _command_entities['track_mode']['value'][0] != 0:
-            _track_speed = _command_entities['track_mode']['value'][1]
-            line_track(_track_speed)
-
-
+        values = _command_entities['track_mode']['value']
+        handle_track_mode(values)
     # obstacle_mode
     # ------------------------------------
     if 'obstacle_mode' in _command_entities.keys():
-        if _command_entities['obstacle_mode']['value'] != commands['obstacle_mode']['last_value']:
-            _obstacle_mode = _command_entities['obstacle_mode']['value']
-            commands['obstacle_mode']['last_value'] = _obstacle_mode
-            if _obstacle_mode[0] == 0:
-                px.stop()
-        if _command_entities['obstacle_mode']['value'][0] != 0:
-            _obstacle_speed = _command_entities['obstacle_mode']['value'][1]
-            avoid_obstacles(_obstacle_speed)
+        values = _command_entities['obstacle_mode']['value']
+        handle_obstacle_mode(values)
 
     # servos_calibration
     # ------------------------------------
     if 'servos_calibration' in _command_entities.keys():
-        if _command_entities['servos_calibration']['value'] != commands['servos_calibration']['last_value']:
-            _servos_calibration = _command_entities['servos_calibration']['value']
-            commands['servos_calibration']['last_value'] = _servos_calibration
-            _servo_index =_servos_calibration[0]
-            _servo_opt = _servos_calibration[1]
-            if _servo_opt == 1:
-                sensors['servos_offset']['value'][_servo_index] += 2
-                if sensors['servos_offset']['value'][_servo_index] > 200:
-                    sensors['servos_offset']['value'][_servo_index] = 200
-            elif _servo_opt == -1:
-                sensors['servos_offset']['value'][_servo_index] -= 2
-                if sensors['servos_offset']['value'][_servo_index] < -200:
-                    sensors['servos_offset']['value'][_servo_index] = -200
-
+        values = _command_entities['servos_calibration']['value']
+        handle_servo_calibation(values)
     # motors calibration
     # ------------------------------------
     if 'motors_calibration' in _command_entities.keys():
-        if _command_entities['motors_calibration']['value'] != commands['motors_calibration']['last_value']:
-            _motors_offset = list(_command_entities['motors_calibration']['value'])
-            commands['motors_calibration']['last_value'] = _motors_offset
-            sensors['motors_offset']['value'] = _motors_offset
-            px.cali_dir_value = list.copy(_motors_offset)
-
+        values = _command_entities['motors_calibration']['value']
+        handle_motors_calibation(values)
 
 async def on_connect(**kwargs):
     # if 'client' in kwargs:
@@ -260,6 +300,7 @@ async def on_connect(**kwargs):
     #     ws.send(DEVICE_INFO, client_id)
     # else:
     #     ws.send(DEVICE_INFO)
+    print(f'on_connected: {kwargs}')
     pass
 
 async def on_disconnect(**kwargs):
@@ -289,28 +330,18 @@ async def on_receive(data, client_id):
 
         # read command
         _command_entities = ws.bytes_to_entities(commands, data)
-        # handle command
-        commands__handler(_command_entities)
+        if isinstance(_command_entities, dict):
+            # handle command
+            commands__handler(_command_entities)
+            with sensor_data_lock:
+                binary_data = ws.entities_to_bytes(sensors)
+            # print(f"Send binary data ({client_id}): {binary_data}")
+            await ws.async_send(binary_data, client_id)
 
-        # send sensors data
-        with sensor_data_lock:
-            binary_data = ws.entities_to_bytes(sensors)
-        print(f"Send binary data ({client_id}): {binary_data}")
-        await ws.asyn_send(binary_data, client_id)
+        else:
+            print(f"Invalid command format ({client_id}): {_command_entities}")
 
-
-def main():
-    global color_detect_result, traffic_sign_detect_result, face_detect_result
-
-    # ws.on_connect = on_connect
-    # ws.on_disconnect = on_disconnect  
-    ws.on_receive = on_receive
-    ws.start()
-    # pause()
-
-    usage_st = time.time()
-
-    while True:
+def update_data():
         ## read sensor data
         ultrasonic_distance = px.get_distance()
         grayscale_data = px.get_grayscale_data()
@@ -370,21 +401,40 @@ def main():
         else:
             sensors['sound_effect_status']['value'] = [0]
 
+        # --- update music position ---
+        if music.get_music_busy():
+            sensors['background_music_pos']['value'][1] = music.get_music_pos()
 
-        with sensor_data_lock:
-            sensors['ultrasonic']['value'] = int(ultrasonic_distance*10)
-            sensors['grayscale']['value'] = grayscale_data
-            sensors['battery_voltage']['value'] = int((battery_voltage-6)*100)
-            sensors['color_detection']['value'] = list.copy(color_detect_result)
-            sensors['face_detection']['value'] = list.copy(face_detect_result)
-            sensors['traffic_sign_detection']['value'] = list.copy(traffic_sign_detect_result)
-            sensors['grayscale_status']['value'] = list.copy(grayscale_status)
+        sensors['ultrasonic']['value'] = int(ultrasonic_distance*10)
+        sensors['grayscale']['value'] = grayscale_data
+        sensors['battery_voltage']['value'] = int((battery_voltage-6)*100)
+        sensors['color_detection']['value'] = list.copy(color_detect_result)
+        sensors['face_detection']['value'] = list.copy(face_detect_result)
+        sensors['traffic_sign_detection']['value'] = list.copy(traffic_sign_detect_result)
+        sensors['grayscale_status']['value'] = list.copy(grayscale_status)
+
+def main():
+
+    # ws.on_connect = on_connect
+    # ws.on_disconnect = on_disconnect  
+    ws.on_receive = on_receive
+    ws.start()
+    # pause()
+
+    usage_st = time.time()
+
+    while True:
         # if time.time() - usage_st > 1:
         #     usage_st = time.time()
 
         #     num_fds = process.num_fds()
         #     memory_usage = process.memory_info().rss / 1024 / 1024  # MB 
         #     print(term.skyblue(f'num_fds: {num_fds} , memory: {memory_usage:.5f} MB'))
+        
+        with sensor_data_lock:
+            update_data()
+        #     binary_data = ws.entities_to_bytes(sensors)
+        # ws.send(binary_data)
         time.sleep(0.02)
 
 
