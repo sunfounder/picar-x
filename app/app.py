0d8f6056d018b45e8d0dbe4763357536d48eca9b
Lovmong <1967471283@qq.com>
2025-06-20 16:42:04 +0800

    feat(volume): add volume data to send in handle_music_volume

--- app/app.py
+++ app/app.py
@@ -374,6 +374,7 @@ def handle_music_control(control):
 def handle_music_volume(volume):
     volume = constrain(volume, 0, 100)
     log.debug(f"Set music volume: {volume}")
+    data_to_send["volume"] = volume
     music.set_music_volume(volume)
 
 def handle_line_tracking(enable):
