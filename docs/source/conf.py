d3d6ac3c0c80000b7eddc3ed943dc5f461838f42
kyuuinochi <kyuuinochi@gmail.com>
2026-07-07 10:53:19 +0800

    docs: update JavaScript and CSS file links to use raw content from GitHub

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,13 +82,13 @@ intersphinx_disabled_reftypes = ["*"]
 #### RTD+
 
 html_js_files = [
-    'https://github.com/sunfounder/rtd-plus/blob/main/custom.js',
+    'https://raw.githubusercontent.com/sunfounder/rtd-plus/refs/heads/main/custom.js',
     './lang.js', # new
     'custom.js', #new
 
 ]
 html_css_files = [
-    'https://github.com/sunfounder/rtd-plus/blob/main/custom.css',
+    'https://raw.githubusercontent.com/sunfounder/rtd-plus/refs/heads/main/custom.css',
 
 ]
 
